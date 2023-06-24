9ef97b3d990d4beb557fad451ea750a9c2679e04
anton <a.tychshenko@infin-it.kz>
2023-06-24 21:23:28 +0600

    JS cart2

--- cart.js
+++ cart.js
@@ -3,6 +3,7 @@ const DOMElementsCart = {
   totalPrizzaMain: document.querySelector(".totalPrizzaMain"),
   totalCountMain: document.querySelector(".totalCountMain"),
   returnMain: document.querySelector(".returnMain"),
+  cart_items: document.querySelector(".cart_items"),
 };
 
 let countPizza = 0;
@@ -63,29 +64,63 @@ function addPizzasCart(name, type, price, id) {
 function displayPizzasCart() {
   let infoPizzas = getPizzasInfoCookie();
 
-  let objPizzas = {};
+  // let objPizzas = {};
+
+  // for (let i = 0; i < infoPizzas.arrayPizzas.length; i++) {
+  //   const elem = infoPizzas.arrayPizzas[i];
+
+  //   let key = elem.split("/")[3];
+
+  //   if (objPizzas[key] !== undefined) {
+  //     objPizzas[key] += 1;
+  //   } else {
+  //     objPizzas[key] = 1;
+  //   }
+  // }
+
+  let items = [];
 
   for (let i = 0; i < infoPizzas.arrayPizzas.length; i++) {
     const elem = infoPizzas.arrayPizzas[i];
 
-    let key = elem.split("/")[3];
+    let name = elem.split("/")[0];
+    let typePizza = elem.split("/")[1];
+    let price = elem.split("/")[2];
 
-    if (objPizzas[key] !== undefined) {
-      objPizzas[key] += 1;
-    } else {
-      objPizzas[key] = 1;
-    }
+    items.push(`<div class="cart_item">
+    <div class="cart_wrapper">
+        <div class="item_info">
+            <img src="" alt="" />
+            <div class="item_info_description">
+                <h1>${name}</h1>
+                <p>${typePizza} тесто</p>
+            </div>
+        </div>
+        <div class="item_count">
+            <button onclick='calcPizzaInCart("-")'>
+                <p>-</p>
+            </button>
+            <p>1</p>
+            <button onclick='calcPizzaInCart("+")'>
+                <p>+</p>
+            </button>
+        </div>
+    </div>
+    <div class="cart_wrapper">
+        <h1 class="item_price">${price} P</h1>
+        <button class="remove" onclick='removeTypsPizzas()'>
+            <img src="../../img/remove.png" alt="remove" />
+        </button>
+    </div>
+  </div>
+    `);
   }
-  console.log(objPizzas);
-
-  let items = [];
-
-  for (let i = 0; i < infoPizzas.length; i++) {
-    const elem = infoPizzas[i];
-  }
+  return items;
 }
 
-displayPizzasCart();
+const pizzasItemCart = displayPizzasCart();
+
+DOMElementsCart.cart_items.innerHTML = pizzasItemCart.join("");
 
 DOMElementsCart.returnMain.addEventListener("click", function () {
   DOMElements.main.classList.remove("none");
